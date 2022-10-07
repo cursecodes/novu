4e60715069204018f4b247d3a3ab69a3b7e6f192
curse <58047023+cursecodes@users.noreply.github.com>
2022-10-07 17:15:05 -0600

    Mailjet webhooks

--- providers/mailjet/src/lib/mailjet.provider.ts
+++ providers/mailjet/src/lib/mailjet.provider.ts
@@ -1,18 +1,20 @@
 import {
   ChannelTypeEnum,
+  CheckIntegrationResponseEnum,
+  EmailEventStatusEnum,
+  ICheckIntegrationResponse,
+  IEmailEventBody,
   IEmailOptions,
   IEmailProvider,
   ISendMessageSuccessResponse,
-  ICheckIntegrationResponse,
-  CheckIntegrationResponseEnum,
-} from '@novu/stateless';
-import Client, { Email } from 'node-mailjet';
-import { MailjetResponse } from './mailjet-response.interface';
+} from "@novu/stateless";
+import Client, { Email } from "node-mailjet";
+import { MailjetResponse } from "./mailjet-response.interface";
 
-const MAILJET_API_VERSION = 'v3.1';
+const MAILJET_API_VERSION = "v3.1";
 
 export class MailjetEmailProvider implements IEmailProvider {
-  id = 'mailjet';
+  id = "mailjet";
   channelType = ChannelTypeEnum.EMAIL as ChannelTypeEnum.EMAIL;
 
   private mailjetClient: Email.Client;
@@ -21,15 +23,15 @@ export class MailjetEmailProvider implements IEmailProvider {
       apiKey: string;
       apiSecret: string;
       from: string;
-    }
+    },
   ) {
     this.mailjetClient = Client.connect(config.apiKey, config.apiSecret);
   }
 
   async sendMessage(
-    options: IEmailOptions
+    options: IEmailOptions,
   ): Promise<ISendMessageSuccessResponse> {
-    const send = this.mailjetClient.post('send', {
+    const send = this.mailjetClient.post("send", {
       version: MAILJET_API_VERSION,
     });
     const requestObject = {
@@ -49,7 +51,7 @@ export class MailjetEmailProvider implements IEmailProvider {
           Attachments: options.attachments?.map((attachment) => ({
             ContentType: attachment.mime,
             Filename: attachment.name,
-            Base64Content: attachment.file.toString('base64'),
+            Base64Content: attachment.file.toString("base64"),
           })),
         },
       ],
@@ -58,18 +60,60 @@ export class MailjetEmailProvider implements IEmailProvider {
     const response = (await send.request(requestObject)) as MailjetResponse;
 
     return {
-      id: response.response.header['x-mj-request-guid'],
+      id: response.response.header["x-mj-request-guid"],
       date: response.response.header.date,
     };
   }
 
   async checkIntegration(
-    options: IEmailOptions
+    options: IEmailOptions,
   ): Promise<ICheckIntegrationResponse> {
     return {
       success: true,
-      message: 'Integrated successfully!',
+      message: "Integrated successfully!",
       code: CheckIntegrationResponseEnum.SUCCESS,
     };
+  }
+
+  parseEventBody(
+    body: any | any[],
+    identifier: string,
+  ): IEmailEventBody | undefined {
+    if (Array.isArray(body)) {
+      body = body.find((item) => item.MessageID === identifier);
+    }
+
+    if (!body) {
+      return undefined;
+    }
+
+    const status = this.getStatus(body.event);
+
+    if (status === undefined) {
+      return undefined;
+    }
+
+    return {
+      status: status,
+      date: new Date().toISOString(),
+      externalId: body.MessageID,
+      response: body.smtp_reply ? body.smtp_reply : "",
+      row: body,
+    };
+  }
+
+  private getStatus(event: string): EmailEventStatusEnum | undefined {
+    switch (event) {
+      case "sent":
+        return EmailEventStatusEnum.DELIVERED;
+      case "open":
+        return EmailEventStatusEnum.OPENED;
+      case "click":
+        return EmailEventStatusEnum.CLICKED;
+      case "bounce":
+        return EmailEventStatusEnum.BOUNCED;
+      case "blocked":
+        return EmailEventStatusEnum.DROPPED;
+    }
   }
 }
